526d7186c24b8cbf18d1e1e635475ffb5000a863
HanSeo0507 <i.hansu.lee@gmail.com>
2022-07-30 17:32:24 +0900

    feat: react-natvie 절대경로 설정

--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,14 @@
  * @format
  */
 
+const path = require('path');
+
 module.exports = {
+  resolver: {
+    extraNodeModules: {
+      src: path.resolve(__dirname, 'src'),
+    },
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
