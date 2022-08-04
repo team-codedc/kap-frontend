25b7e769924f4251a9aa25ab9d74cb8db382d5c2
HanSeo0507 <i.hansu.lee@gmail.com>
2022-08-04 12:15:38 +0900

    chore: eslint react-hooks 관련 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
       },
     },
   ],
