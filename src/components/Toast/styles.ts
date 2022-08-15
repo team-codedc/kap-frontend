d4c582b07485f4db61f6309e11580eda1eba0d75
HanSeo0507 <i.hansu.lee@gmail.com>
2022-08-16 03:55:53 +0900

    feat: react-natvie-toast-message 커스텀 컴포넌트 추가

---
+++ src/components/Toast/styles.ts
@@ -0,0 +1,38 @@
+import {StyleSheet} from 'react-native';
+
+import {SpoqaHanSansNeo} from 'src/styles';
+import {fp, hp, wp} from 'src/utils';
+
+export const styles = StyleSheet.create({
+  contentContainer: {
+    marginTop: hp(1),
+    paddingHorizontal: wp(17),
+  },
+  toastText1: {
+    color: '#282727',
+    fontSize: fp(14),
+    fontFamily: SpoqaHanSansNeo.bold,
+    letterSpacing: fp(-0.1),
+  },
+  toastText2: {
+    color: '#7C7A80',
+    fontSize: fp(12),
+    fontFamily: SpoqaHanSansNeo.regular,
+    marginTop: hp(-1),
+  },
+  baseToast: {
+    marginTop: hp(15),
+  },
+  successToast: {
+    borderLeftColor: '#4ABB4F',
+  },
+  errorToast: {
+    borderLeftColor: '#F1574D',
+  },
+  infoToast: {
+    borderLeftColor: 'skyblue',
+  },
+  warningToast: {
+    borderLeftColor: '#E9AE00',
+  },
+});
