d5cde4f277904ef86d150d8ac6e938e79eb47500
HanSeo0507 <i.hansu.lee@gmail.com>
2022-08-20 15:39:45 +0900

    fix: 소셜로그인 성공 후 프로필 fetch 실패 이슈 해결

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,12 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useQueryClient} from '@tanstack/react-query';
 import React from 'react';
 import {Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {useSetRecoilState} from 'recoil';
+import {Button} from 'src/components';
+import {globalAccessTokenState} from 'src/store';
 
 export const HomeScreen: React.FC = () => {
+  const queryClient = useQueryClient();
+  const setGlobalAccessToken = useSetRecoilState(globalAccessTokenState);
+
+  const clearRefreshToken = async () => {
+    queryClient.getQueryCache().clear();
+    setGlobalAccessToken('');
+    await AsyncStorage.setItem('@token', '');
+  };
+
   return (
     <SafeAreaView>
       <View>
         <Text>Hello, World!</Text>
+        <Button label="로그아웃" onPress={clearRefreshToken} />
       </View>
     </SafeAreaView>
   );
