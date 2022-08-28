60001c9fd9eb230fc4a78383b58ee77b0fce766e
Comet105 <taegyeon512@naver.com>
2022-08-28 18:20:38 +0900

    feat: GlobalNavigationBar에 navigate 기능 추가

--- src/components/GlobalNavigationBar/index.tsx
+++ src/components/GlobalNavigationBar/index.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 import {GLOBAL_NAVIGATION_BAR} from 'src/constant';
+import {useNavigation} from 'src/hooks';
 import {styles} from './styles';
 
 export const GlobalNavigationBar: React.FC = () => {
+  const {navigate} = useNavigation();
+
   return (
     <SafeAreaView>
       <View style={styles.globalNavigationBarWrapper}>
         {GLOBAL_NAVIGATION_BAR.map(content => {
           return (
-            <View style={styles.globalNavigationBarContentsWrapper}>
-              <Image
-                source={content.icon}
-                style={styles.globalNavigationBarIcon}
-              />
-              <Text style={styles.globalNavigationBarDescription}>
-                {content.text}
-              </Text>
-            </View>
+            <TouchableOpacity onPress={() => navigate(content.id)}>
+              <View style={styles.globalNavigationBarContentsWrapper}>
+                <Image
+                  source={content.icon}
+                  style={styles.globalNavigationBarIcon}
+                />
+                <Text style={styles.globalNavigationBarDescription}>
+                  {content.text}
+                </Text>
+              </View>
+            </TouchableOpacity>
           );
         })}
       </View>
