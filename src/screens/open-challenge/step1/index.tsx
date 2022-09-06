9b8a04070d3c786a0d047c5efcfdd6af5062417d
최민기 <cmg8431@gmail.com>
2022-09-06 18:56:31 +0900

    feat: Step1 UI 구현

--- src/screens/open-challenge/step1/index.tsx
+++ src/screens/open-challenge/step1/index.tsx
@@ -1,12 +1,67 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {globalStyles} from 'src/styles';
+import {styles} from './styles';
 
 export const OpenChallengeStep1Screen: React.FC = () => {
+  const CHALLENGE_CATEGORY_ITEMS = [
+    {
+      text: '나만의 카테고리로 시작할래요!',
+      icon: require('src/assets/3d-fluency-idea.png'),
+      category: 'etc',
+    },
+    {
+      text: '분리수거 챌린지를 만들고 싶어요!',
+      icon: require('src/assets/3d-fluency-idea.png'),
+      category: 'separateTheTrash',
+    },
+    {
+      text: '걷기 챌린지를 만들고 싶어요!',
+      icon: require('src/assets/3d-fluency-idea.png'),
+      category: 'walking',
+    },
+    {
+      text: '샴푸 줄이기 챌린지를 만들고 싶어요!',
+      icon: require('src/assets/3d-fluency-idea.png'),
+      category: 'reduceShampoo',
+    },
+  ];
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello, World!</Text>
+    <SafeAreaView style={globalStyles.safeAreaView}>
+      <View style={globalStyles.screenContainer}>
+        <View style={styles.openChallengeContainer}>
+          <Text style={styles.onChallengeNewCaption}>new</Text>
+          <Text style={styles.openChallengeBoldText}>
+            유저들과 함께 환경을 지킬
+          </Text>
+          <Text style={styles.openChallengeRegularText}>
+            미션을 만들어봐요!
+          </Text>
+        </View>
+        <View style={styles.openChallengeContainer}>
+          {CHALLENGE_CATEGORY_ITEMS.map(item => {
+            return (
+              <TouchableOpacity activeOpacity={0.6}>
+                <Text
+                  numberOfLines={1}
+                  ellipsizeMode="tail"
+                  style={styles.openChallengeCategoryButton}>
+                  <Image source={item.icon} />
+                  {item.text}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.openChallengeContainer}>
+          <Text style={styles.openChallengeCategory}>
+            다른 환경 미션 카테고리를 만들고싶으면?
+          </Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
--- src/screens/open-challenge/step1/styles.ts
+++ src/screens/open-challenge/step1/styles.ts
@@ -1,1 +1,42 @@
-export {};
+import {StyleSheet} from 'react-native';
+import {colors, SpoqaHanSansNeo} from 'src/styles';
+import {fp, hp, wp} from 'src/utils';
+
+export const styles = StyleSheet.create({
+  openChallengeContainer: {alignItems: 'center'},
+  openChallengeBoldText: {
+    fontFamily: SpoqaHanSansNeo.bold,
+    color: colors.text.primary,
+    fontSize: fp(25),
+    letterSpacing: wp(-0.8),
+  },
+  openChallengeRegularText: {
+    fontFamily: SpoqaHanSansNeo.regular,
+    color: colors.text.primary,
+    fontSize: fp(25),
+    letterSpacing: wp(-0.8),
+  },
+  openChallengeCategoryButton: {
+    fontFamily: SpoqaHanSansNeo.medium,
+    width: wp(280),
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    backgroundColor: '#f8f8f8',
+    borderRadius: wp(100),
+    paddingVertical: hp(23),
+    paddingHorizontal: wp(30),
+    marginTop: hp(25),
+    color: colors.text.primary,
+    fontSize: fp(16),
+    letterSpacing: wp(-0.5),
+  },
+  openChallengeCategory: {
+    marginTop: hp(40),
+    textDecorationLine: 'underline',
+    textUnderlinePosition: 'under',
+  },
+  onChallengeNewCaption: {
+    backgroundColor: 'red',
+  },
+});
