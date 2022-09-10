f5d7002a3de5b2c84710e931aa3581cd612666bd
gull2365 <jych2365@gmail.com>
2022-09-10 22:41:34 +0900

    feat: privacy 컴포넌트 기본 레이아웃 추가

---
+++ src/components/layout/AppLayout/styles.ts
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+import {hp, wp} from 'src/utils';
+
+export const styles = StyleSheet.create({
+  appLayoutContainer: {
+    flex: 2,
+  },
+  appLayoutWrapper: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingRight: wp(25),
+    paddingLeft: wp(25),
+    borderRadius: 17,
+  },
+  layout_exit_button: {
+    width: wp(29),
+    height: hp(29),
+    top: hp(20),
+  },
+});
