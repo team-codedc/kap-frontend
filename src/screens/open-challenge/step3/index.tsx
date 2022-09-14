07d57976f0cd725799614dc7444caaadb24332cc
cmg8431 <cmg8431@gmail.com>
2022-09-15 00:03:55 +0900

    feat: React-hook-form을 이용하여 챌린지 생성 폼 제작

--- src/screens/open-challenge/step3/index.tsx
+++ src/screens/open-challenge/step3/index.tsx
@@ -1,12 +1,73 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {TextInput, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {useForm, Controller} from 'react-hook-form';
+import {Button, TextArea} from 'src/components';
+
+type OpenChallengeStep3Values = {
+  name: string;
+  description: string;
+  rule: string;
+  certificationFrequency: any;
+  certificationPerDay: string;
+  certificableStartTime: string;
+  certificableFinishTime: string;
+  category: string[];
+};
 
 export const OpenChallengeStep3Screen: React.FC = () => {
+  const {handleSubmit, control} = useForm<OpenChallengeStep3Values>();
+  const onSubmit = (data: OpenChallengeStep3Values) => {
+    console.log(data);
+  };
+
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello, World!</Text>
+      <View style={{padding: 20}}>
+        <Controller
+          control={control}
+          render={({field: {onChange, onBlur, value}}) => (
+            <TextInput
+              onBlur={onBlur}
+              onChangeText={v => onChange(v)}
+              value={value}
+              placeholder="챌린지명 (예: 한강 쓰레기 분리수거)"
+            />
+          )}
+          name="name"
+          rules={{required: true}}
+        />
+        <Controller
+          control={control}
+          render={({field: {onChange, onBlur, value}}) => (
+            <TextArea
+              onBlur={onBlur}
+              onChangeText={v => onChange(v)}
+              value={value}
+              label="챌린지 설명"
+              numberOfLines={5}
+              placeholder="무슨 챌린지를 진행하는지 자세하게 입력해주세요! &#10;(30자 이상)"
+            />
+          )}
+          name="description"
+          rules={{required: true}}
+        />
+        <Controller
+          control={control}
+          render={({field: {onChange, onBlur, value}}) => (
+            <TextArea
+              onBlur={onBlur}
+              onChangeText={v => onChange(v)}
+              value={value}
+              label="인증 규칙"
+              numberOfLines={5}
+              placeholder="규칙을 입력해 보다 정확한 인증에 도전해봐요 :) &#10;(30자 이상)"
+            />
+          )}
+          name="rule"
+          rules={{required: true}}
+        />
+        <Button onPress={handleSubmit(onSubmit)} label="생성하기" />
       </View>
     </SafeAreaView>
   );
