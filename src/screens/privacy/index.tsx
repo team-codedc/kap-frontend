c8310c7acbe8760787e532b0a81a919d733ff292
gull2365 <jych2365@gmail.com>
2022-09-14 23:35:32 +0900

    feat: 스크린 타이틀 삭제

--- src/screens/privacy/index.tsx
+++ src/screens/privacy/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 import {AppLayout, Button, Input} from 'src/components';
 import {styles} from './styles';
 
 export const PrivacyScreen: React.FC = () => {
   return (
-    <AppLayout>
-      <Text style={styles.privacyPageTitle}>개인정보 입력</Text>
+    <AppLayout title="개인정보입력">
       <Input label="닉네임" />
       <Input label="이메일" />
       <Input label="생년월일" />
