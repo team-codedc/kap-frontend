3db01b017bb07073c407604403a22932d68a7463
Comet105 <taegyeon512@naver.com>
2022-09-21 00:06:56 +0900

    feat: navigate 에 따라 GNB Icon 및 Text 색 변경

--- src/components/GlobalNavigationBar/styles.ts
+++ src/components/GlobalNavigationBar/styles.ts
@@ -1,18 +1,23 @@
 import {StyleSheet} from 'react-native';
-import {SpoqaHanSansNeo} from 'src/styles';
+import {SpoqaHanSansNeo, colors} from 'src/styles';
 import {fp, hp, wp} from 'src/utils';
 
 export const styles = StyleSheet.create({
   globalNavigationBarWrapper: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: '#ffffff',
-    paddingTop: hp(22),
-    paddingBottom: hp(22),
+    paddingTop: hp(16),
+    paddingBottom: hp(16),
     paddingLeft: wp(30),
-    paddingRight: wp(20),
-    borderRadius: 17,
+    paddingRight: wp(22),
+    borderTopRightRadius: 17,
+    borderTopLeftRadius: 17,
     borderTopColor: '#F0F0F0',
     borderTopWidth: 1,
     borderStartColor: '#F0F0F0',
@@ -29,7 +34,34 @@ export const styles = StyleSheet.create({
   },
   globalNavigationBarDescription: {
     paddingTop: hp(2),
-    fontSize: fp(10),
+    fontSize: fp(12),
     fontFamily: SpoqaHanSansNeo.bold,
+    color: '#C9C9C9',
+  },
+  globalNavigationBarDescriptionFocused: {
+    color: '#282727',
+  },
+  globalNavigationBarFlatListItemContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  globalNavigationBarFlatListItemIcon: {
+    width: wp(20),
+    height: hp(20),
+    marginBottom: hp(10),
+  },
+  globalNavigationBarFlatListItemText: {
+    fontFamily: SpoqaHanSansNeo.bold,
+    fontSize: fp(22),
+    textAlign: 'center',
+    color: colors.text.primary,
+    marginBottom: hp(9),
+  },
+  globalNavigationBarFlatListItemDescription: {
+    fontFamily: SpoqaHanSansNeo.regular,
+    fontSize: fp(17),
+    textAlign: 'center',
+    color: colors.text.primary,
   },
 });
